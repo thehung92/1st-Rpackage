c21d8bc3f17af5db1e86fa636a1c50233623c9a8
Hung <thehung92phuyen@gmail.com>
2022-07-22 12:04:57 +0800

    v0.2.0: ggplot theme

---
+++ script-git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# create git version control in existing folder
+# cd $working_dir
+git init
+git add .
+git commit -m 'my first commit' -a v0.1.0
+git remote add origin https://github.com/thehung92/misc-Rpackage.git
+git branch -M  "main"
+git push -u origin "main"
+# check current remote
+# remove remote to point to another git
+# git remote rm origin
+git remote -v
